2570f01f4d49bab7fb6b3a7d08c77f1f06af0846
userkmitl <59050300@kmitl.ac.th>
2018-02-14 14:54:42 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var unirest = require('unirest');
+
+
+var GroupID = ""
+var Token = ""
+
+DelPostroup();
+
+function DelPostroup(){
+count = 0 ;
+unirest.get('https://graph.facebook.com/v2.12/'+GroupID+'/feed?access_token='+Token)
+.header({ 'Accept': 'application/json','Content-Type': 'application/json' })
+.end(async function (response) {
+   response.body.data.forEach(async function (Idpost) {
+    unirest.delete('https://graph.facebook.com/v2.12/' +Idpost.id+'/?access_token='+Token)
+    .header({ 'Accept': 'application/json','Content-Type': 'application/json' })
+    .end(await function (response2) {
+        console.log("DELETE POST : "+ Idpost.id +" Response : " + response2.body.success);
+        count++;
+    })
+})
+});
+if (count > 5){
+    DelPostroup();
+}
+}
